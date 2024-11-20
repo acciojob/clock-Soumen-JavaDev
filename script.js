5ebe619998205e06be4f4607cefea11d10caaab5
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-11-20 12:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let display=document.getElementById('timer');
+
+function time() {
+	let currtime=new Date();
+
+	display.innerHTML =currtime.toLocaleString();
+	
+}
+setInterval(time,1000);
+time();
